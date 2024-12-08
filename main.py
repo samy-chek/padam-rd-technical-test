f187681b06e3da321b765eb2fc0ca9e1bba1739a
samy-chek <samy.chekkouri@gmail.com>
2024-12-08 15:35:28 +0100

    commit1

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     in_file, plot_graph = parse_cmd_line()
     vertices, edges = parse_file(in_file)
     print(f"#E={len(edges)}, #V={len(vertices)}")
+    print("on est ici")
     graph = Graph(vertices, edges)
     if plot_graph:
         graph.plot()
@@ -13,3 +14,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
