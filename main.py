81d1e9a83700c413a0cb852ace2a619e1fc10ab4
samy-chek <samy.chekkouri@gmail.com>
2024-12-09 13:18:05 +0100

    commit print

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ def main():
     in_file, plot_graph = parse_cmd_line()
     vertices, edges = parse_file(in_file)
     print(f"#E={len(edges)}, #V={len(vertices)}")
-    print("on est ici là")
+
     graph = Graph(vertices, edges)
+    
+    for vertice in graph.vertices:
+        print(vertice)
     
     
     # On trouve un chemin qui passe par toutes les arêtes
-    path = graph.find_path()
+    #path = graph.find_path()
     
     if plot_graph:
         graph.plot()
